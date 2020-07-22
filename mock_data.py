24c2ade8d07e4d9920c850f797acc7a241ba87f7
Edmund-Shi <shiyuezju@gmail.com>
2020-07-22 22:22:32 +0800

    sort param in dataset

--- mock_data.py
+++ mock_data.py
@@ -1,24 +1,30 @@
 import csv
 
 
+def sort_params(params_str):
+    params = params_str.split(',')
+    return ','.join(sorted(params))
+
+
 def get_dataset():
     filename = "export.csv"
     dataset = {}
-    header = []
     with open(filename, encoding='utf-8') as f:
         reader = csv.reader(f)
         for row in reader:
             uid, command, params, os_type, source, begin, end = row
+            params = sort_params(params)
             if reader.line_num == 1:
-                header = row
                 continue
             if uid not in dataset:
-                dataset[uid] = [row[1:]]
+                dataset[uid] = [(command, params, os_type, source, begin, end)]
             else:
                 last_cmd, last_params, *_ = dataset[uid][-1]
+                last_params = sort_params(last_params)
                 if last_cmd == command and last_params == params:
                     continue  # duplicate
-                dataset[uid].append(row[1:])
+                dataset[uid].append(
+                    (command, params, os_type, source, begin, end))
     return dataset
 
 
--- recommend.py
+++ recommend.py
@@ -1,6 +1,10 @@
 import mock_data
 from command_table import CommandTable
 
+def print_cmd(nx_cmd):
+    cmd, params, confidence = nx_cmd
+    print("{}% people use run this command: {} {}".format(confidence*100, cmd, ' '.join(params.split(','))))
+
 
 def main():
     dataset = mock_data.get_dataset()
@@ -9,7 +13,8 @@ def main():
     nx_cmd = cmd_table.get_next_command(
         "account get-access-token", "-o,--resource")
     nx_cmd = cmd_table.get_next_command("vm create", "")
-    print(nx_cmd)
+    print_cmd(nx_cmd)
+    print_cmd(cmd_table.get_next_command("webapp create", ""))
 
 
 if __name__ == "__main__":
